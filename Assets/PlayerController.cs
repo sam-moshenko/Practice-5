b5a3c346beebbed9030b386f18b99697d1204806
Semen Lebedev <lebedev_s@au.kg>
2016-12-01 09:41:42 +0600

    Part of the project

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	NavMeshAgent agent; 
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+			RaycastHit result;
+			if (Physics.Raycast (ray, out result, 100)) {
+				agent.SetDestination (result.point);
+			}
+		}
+	}
+}
